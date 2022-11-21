dd5ea9ed6a3568538e1dcd6c190432059997ab6e
sunzhikai <sunzhikai2001@163.com>
2022-11-21 16:35:18 +0800

    commit 1

---
+++ src/main/java/main.java
@@ -0,0 +1,10 @@
+/**
+ * @author Sunzhikai
+ * @version 1.0
+ * @date 2022/11/21 16:34
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("qwwqw");
+    }
+}
